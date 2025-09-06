329c7999bf28009674e1dac921b466e48597f1c7
Alejandro Raffo <amsterdamAL@gmail.com>
2025-09-06 16:56:20 -0400

    add css reset

--- app/javascript/genesis.js
+++ app/javascript/genesis.js
@@ -1,12 +1,13 @@
-import React from "react";
-import Navbar from "./navbar";
+import React from "react"
+import Navbar from "./navbar"
+import "../assets/stylesheets/application.css"
 
 const Genesis = () => {
-  return (
-    <div>
-      <Navbar />
-    </div>
-  );
-};
+	return (
+		<div>
+			<Navbar />
+		</div>
+	)
+}
 
-export default Genesis;
+export default Genesis
